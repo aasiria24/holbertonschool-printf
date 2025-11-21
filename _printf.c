9dfec46833a8978a47b846b39591ad0273d57bb9
i1SaSa <s9ny398335@gmail.com>
2025-11-21 22:58:13 +0000

    Fix if statement

--- _printf.c
+++ _printf.c
@@ -41,26 +41,26 @@ int _printf(const char *format, ...)
 			{
 				count += print_binary(va_arg(args, unsigned int));
 			}
+			else if (*format == 'u')
+                        {
+                                count += print_unsigned(va_arg(args, unsigned int));
+                        }
+                        else if (*format == 'o')
+                        {
+                                count += print_octal(va_arg(args, unsigned int));
+                        }
+                        else if (*format == 'x')
+                        {
+                                count += print_hex_lower(va_arg(args, unsigned int));
+                        }
+                        else if (*format == 'X')
+                        {
+                                count += print_hex_upper(va_arg(args, unsigned int));
+                        }
 			else
 			{
 				count += _putchar('%');
 				count += _putchar(*format);
-			}
-			else if (*format == 'u')
-			{
-				count += print_unsigned(va_arg(args, unsigned int));
-			}
-			else if (*format == 'o')
-			{
-				count += print_octal(va_arg(args, unsigned int));
-			}
-			else if (*format == 'x')
-			{
-				count += print_hex_lower(va_arg(args, unsigned int));
-			}
-			else if (*format == 'X')
-			{
-				count += print_hex_upper(va_arg(args, unsigned int));
 			}
 		else
 		{
