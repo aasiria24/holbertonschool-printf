8721e64457b1c420b3aa683326c7b18ea79d2e11
i1SaSa <s9ny398335@gmail.com>
2025-11-21 13:16:38 +0000

    Add statement for binary format

--- _printf.c
+++ _printf.c
@@ -8,48 +8,53 @@
 */
 int _printf(const char *format, ...)
 {
-va_list args;
-int count = 0;
+	va_list args;
+	int count = 0;
 
-if (!format || (format[0] == '%' && format[1] == '\0'))
-return (-1);
+	if (!format || (format[0] == '%' && format[1] == '\0'))
+	return (-1);
 
-va_start(args, format);
+	va_start(args, format);
 
-while (*format)
-{
-if (*format == '%')
-{
-format++;
-if (*format == 'c')
-{
-count += _putchar(va_arg(args, int));
-}
-else if (*format == 's')
-{
-count += print_string(va_arg(args, char *));
-}
-else if (*format == '%')
-{
-count += _putchar('%');
-}
-else if (*format == 'd' || *format == 'i')
-{
-count += print_number(va_arg(args, int));
-}
-else
-{
-count += _putchar('%');
-count += _putchar(*format);
-}
-}
-else
-{
-count += _putchar(*format);
-}
-format++;
-}
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == 'c')
+			{
+				count += _putchar(va_arg(args, int));
+			}
+			else if (*format == 's')
+			{
+				count += print_string(va_arg(args, char *));
+			}
+			else if (*format == '%')
+			{
+				count += _putchar('%');
+			}
+			else if (*format == 'd' || *format == 'i')
+			{
+				count += print_number(va_arg(args, int));
+			}
+			else if (format[i] == 'b')
+			{
+				count += print_binary(va_arg(args, unsigned int));
+			}
 
-va_end(args);
-return (count);
+		else
+		{
+			count += _putchar('%');
+			count += _putchar(*format);
+		}
+		}
+		else
+		{
+			count += _putchar(*format);
+		}
+		format++;
+	}
+
+	va_end(args);
+	return (count);
 }
